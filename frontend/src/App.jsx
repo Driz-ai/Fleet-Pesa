c873ad45b0ca149ad379126d7b13c149fa5495e9
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-08-20 16:32:33 +0300

    Fix import path to match ft/owner-dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import Driver from "./components/Dashboard/Driver.jsx";
+import { AppShell } from "./components/layout/AppShell.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 export default function App() {
-  return <LoginPage />;
+
+  return ( 
+    <>
+  <LoginPage />
+  <AppShell/>
+  <Driver/>
+</>
+
+  )
+
 }
--- frontend/src/components/Dashboard/Owner.jsx
+++ frontend/src/components/Dashboard/Owner.jsx
@@ -1,4 +1,4 @@
-                                                                                                                                                                                                                                                                                                import {
+ import {
 	CartesianGrid,
 	Line,
 	LineChart,
