fc46bd71d21d6b388b122c726cfcd6dd49a6e64a
fredricknyamweya-del <fredrick.nyamweya@student.moringaschool.com>
2026-08-28 18:47:51 +0300

    Fare Payment Schema

--- backend/routes/fare_payment_routes.py
+++ backend/routes/fare_payment_routes.py
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import hmac
 from uuid import uuid4
 
 from flask import current_app, request
-=======
-from flask import request
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource
 from marshmallow import ValidationError
 
 from extensions import db
-<<<<<<< HEAD
-=======
-from models.driver_assignment import DriverAssignment
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
 from models.fare_payment import FarePayment
 from models.user import User
 from models.vehicle import Vehicle
@@ -23,84 +15,29 @@ from schemas.fare_payment_schema import (
     fare_payment_create_schema,
     fare_payment_schema,
 )
-<<<<<<< HEAD
 from utils.access_control import _can_access_vehicle, _can_read_transaction
-=======
-
-
-def _current_user():
-    return db.session.get(User, int(get_jwt_identity()))
-
-
-def _can_access_vehicle(user, vehicle):
-    if user is None or vehicle is None:
-        return False
-    if user.role == "admin" and user.fleet_owner_id == vehicle.fleet_owner_id:
-        return True
-    return DriverAssignment.query.filter_by(
-        vehicle_id=vehicle.id,
-        driver_id=user.id,
-        unassigned_at=None,
-    ).first() is not None
-
-
-class FarePaymentList(Resource):
-    @jwt_required()
-    def post(self):
-        user = _current_user()
-        if user is None:
-            return {"message": "User not found"}, 404
-        try:
-            data = fare_payment_create_schema.load(
-                request.get_json(silent=True) or {}
-            )
-        except ValidationError as error:
-            return {"message": "Invalid fare payment data", "errors": error.messages}, 400
-
-        vehicle = db.session.get(Vehicle, data["vehicle_id"])
-        if vehicle is None:
-            return {"message": "Vehicle not found"}, 404
-        if not _can_access_vehicle(user, vehicle):
-            return {"message": "You do not have access to this vehicle"}, 403
-
-        payment = FarePayment(**data)
-        db.session.add(payment)
-        db.session.commit()
-        return {"fare_payment": fare_payment_schema.dump(payment)}, 201
-
-
-class FarePaymentDetail(Resource):
-    @jwt_required()
-    def get(self, payment_id):
-        user = _current_user()
-        payment = db.session.get(FarePayment, payment_id)
-        if payment is None:
-            return {"message": "Fare payment not found"}, 404
-        vehicle = db.session.get(Vehicle, payment.vehicle_id)
-        if not _can_access_vehicle(user, vehicle):
-            return {"message": "You do not have access to this fare payment"}, 403
-        return {"fare_payment": fare_payment_schema.dump(payment)}, 200
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
 
 
 class FarePaymentCallback(Resource):
     def post(self):
-<<<<<<< HEAD
         configured_secret = current_app.config.get("MPESA_CALLBACK_SECRET")
         received_secret = request.headers.get("X-MPESA-CALLBACK-SECRET", "")
         if not configured_secret:
-            return {"message": "M-Pesa callback authentication is not configured"}, 503
+            return {
+                "message": "M-Pesa callback authentication is not configured"
+            }, 503
         if not hmac.compare_digest(received_secret, configured_secret):
             return {"message": "Invalid M-Pesa callback signature"}, 401
 
-=======
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
         try:
             data = fare_payment_callback_schema.load(
                 request.get_json(silent=True) or {}
             )
         except ValidationError as error:
-            return {"message": "Invalid M-Pesa callback", "errors": error.messages}, 400
+            return {
+                "message": "Invalid M-Pesa callback",
+                "errors": error.messages,
+            }, 400
 
         payment = FarePayment.query.filter_by(
             mpesa_reference=data["mpesa_reference"]
@@ -111,7 +48,6 @@ class FarePaymentCallback(Resource):
         payment.mpesa_transaction_code = data["mpesa_transaction_code"]
         payment.payment_status = data["payment_status"]
         db.session.commit()
-<<<<<<< HEAD
         return {"fare_payment": payment.to_dict()}, 200
 
 
@@ -159,7 +95,7 @@ class FarePaymentDetail(Resource):
             return {"message": "Fare payment not found"}, 404
         vehicle = db.session.get(Vehicle, payment.vehicle_id)
         if not _can_read_transaction(user, vehicle, payment.requested_at):
-            return {"message": "You do not have access to this fare payment"}, 403
-=======
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
+            return {
+                "message": "You do not have access to this fare payment"
+            }, 403
         return {"fare_payment": fare_payment_schema.dump(payment)}, 200
--- backend/schemas/fare_payment_schema.py
+++ backend/schemas/fare_payment_schema.py
@@ -1,22 +1,6 @@
 from marshmallow import Schema, fields, validate
 
 
-<<<<<<< HEAD
-class FarePaymentCallbackSchema(Schema):
-    mpesa_reference = fields.Str(required=True, validate=validate.Length(max=100))
-    mpesa_transaction_code = fields.Str(required=True, validate=validate.Length(max=100))
-    payment_status = fields.Str(
-        required=False,
-        load_default="confirmed",
-        validate=validate.OneOf(("pending", "confirmed", "failed")),
-    )
-
-
-fare_payment_callback_schema = FarePaymentCallbackSchema()
-
-
-=======
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
 class FarePaymentCreateSchema(Schema):
     vehicle_id = fields.Int(required=True, validate=validate.Range(min=1))
     customer_phone = fields.Str(required=True, validate=validate.Length(max=15))
@@ -26,8 +10,6 @@ class FarePaymentCreateSchema(Schema):
         as_string=False,
         validate=validate.Range(min=0),
     )
-<<<<<<< HEAD
-=======
     mpesa_reference = fields.Str(
         required=False,
         allow_none=True,
@@ -58,7 +40,6 @@ class FarePaymentCallbackSchema(Schema):
         load_default="confirmed",
         validate=validate.OneOf(("pending", "confirmed", "failed")),
     )
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
 
 
 class FarePaymentSchema(Schema):
@@ -73,11 +54,7 @@ class FarePaymentSchema(Schema):
 
 
 fare_payment_create_schema = FarePaymentCreateSchema()
-<<<<<<< HEAD
-fare_payment_schema = FarePaymentSchema()
-=======
 fare_payment_update_schema = FarePaymentUpdateSchema()
 fare_payment_callback_schema = FarePaymentCallbackSchema()
 fare_payment_schema = FarePaymentSchema()
 fare_payments_schema = FarePaymentSchema(many=True)
->>>>>>> 96670e7b8a56aed76fc5ca1becc53292eefc051d
