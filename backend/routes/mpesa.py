4b2ca21d8a0d8232e9a99efe4d5692eff180ef45
Sam Tomashi <samtomashikayembi@yhaoo.fr>
2026-09-01 15:25:54 +0300

    add sender and receiver phone number

--- backend/routes/mpesa.py
+++ backend/routes/mpesa.py
@@ -13,12 +13,14 @@ class Mpesa(Resource):
     def post(self):
         data = request.get_json()
 
-        phone_number = data["phone_number"]
+        sender_phone_number = data["sender_phone_number"]
+        receiver_phone_number = data["receiver_phone_number"]
         amount = data["amount"]
         account_reference = data["account_reference"]
 
         result = mpesa.stk_push(
-            phone_number=phone_number,
+            sender_phone_number=sender_phone_number,
+            receiver_phone_number=receiver_phone_number,
             amount=amount,
             account_reference=account_reference
         )
